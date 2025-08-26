9e19b126e54e1443baf5fcc2fc73826c749d2ef7
= <gohelbhargav401@gmail.com>
2025-08-27 01:13:17 +0530

    problem controllers

--- backend/src/controllers/problem.controller.js
+++ backend/src/controllers/problem.controller.js
@@ -1,6 +1,22 @@
 
+import { UserRole } from "../generated/prisma/index.js";
 import { db } from "../libs/db.js"
+import { ApiError } from "../utills/api-error.js";
 export const createProblem = async(req,res) => {
+
+    const { title,description,difficulty,tag,examples,constarints,testcases,codeSnippets,referenceSolutions } = req.body;
+
+    //check if not admin
+    if(req.user.role !== UserRole.ADMIN){
+        return res.status(403).json(new ApiError(403,"You are not allowed to create problem"))
+    }
+
+    try {
+
+        
+    } catch (error) {
+        
+    }
 
 }
 
